ec99d74d19dc00f36be1a35a65a0a684fcba00d0
wang <wangxuezhi@wdai.com>
2019-09-10 13:24:28 +0800

    修复生成微信小程序接口中增options参数没传的bug

--- tpl/api/wxa.js
+++ tpl/api/wxa.js
@@ -3,8 +3,8 @@ exports.__api_name__ = function (options) {
   return network.request({
     method: __method__,
     url: __url__,
-    data: options.data || {},
-    header: options.header || {},
+    data: (options && options.data) || {},
+    header: (options && options.header) || {},
     apiPackageName: __apiPackageName__
   })
 }
